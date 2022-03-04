4eb6aad244cff4b8a4262ab11addfa0680628903
nursultan25 <nursultankudajkulov11@gmail.com>
2022-03-04 18:22:58 +0600

    first commit

---
+++ src/main/java/com/example/ecommerce_online/ECommerceOnlineApplication.java
@@ -0,0 +1,13 @@
+package com.example.ecommerce_online;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceOnlineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ECommerceOnlineApplication.class, args);
+    }
+
+}
